23b1818e9ed34b6cac525ef62269052fd93cd680
github-roshini <109035217+github-roshini@users.noreply.github.com>
2024-10-09 14:57:28 +0530

    notes

---
+++ src/components/Modal/Title.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import React from 'react';
+import "./Css/Title.css";
+const Title = ({title="Not found",color="black"}) => {
+
+    const [logoName,setLogoName] = useState('')
+
+    useEffect(() => {
+        let words = title.split(" ");
+        let initials = '';
+    
+        if (words.length > 0) {
+          initials += words[0][0];
+        }
+    
+        if (words.length > 1) {
+          initials += words[1][0];
+        }
+    
+        setLogoName(initials.toUpperCase());
+      }, [title]);
+
+  return <div className="title-component">
+    <div className="logo" style={{backgroundColor: color ? color : "#fff"}}>{logoName}</div>
+    <div className="note-title">{title}</div>
+  </div>;
+};
+
+export default Title;
