7e3342c72997c76160ccb1f64f14974322080c85
github-roshini <109035217+github-roshini@users.noreply.github.com>
2024-10-09 15:02:59 +0530

    pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { IoMdLock } from "react-icons/io";
+import '../components/Css/Home.css'
+
+
+const Home = () => {
+  return (
+    <div className="right" >
+        <div className="right-wrapper">
+          <img className="home-image" src="/Home.png" alt="home-img" />
+          <h1>Pocket Notes</h1>
+          <p>
+            Send and receive messages without keeping your phone online. Use
+            Pocket Notes on up to 4 linked devices and 1 mobile phone
+          </p>
+        </div>
+        <div className="footer">
+          <span><IoMdLock /> end-to-end encrypted</span>
+        </div>
+      </div>
+  )
+}
+
+export default Home
---
+++ src/pages/SubNotes.jsx
@@ -0,0 +1,81 @@
+import { useParams, useNavigate } from "react-router-dom";
+import React from 'react';
+import useDateTime from "../hooks/useDateTime";
+import Card from "../components/Card";
+import { useSelector, useDispatch } from "react-redux";
+import { addSubNote } from "../slices/notesslice";
+import { IoSend } from "react-icons/io5";
+import "../components/Css/SubNotes.css";
+import { useState } from "react";
+import Title from "../components/Title";
+import useWindowWidth from "../hooks/useWindowWidth"; // Import the hook
+
+const SubNotes = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { date, time } = useDateTime();
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state.notes.notes);
+  const currentNote = notes.find((note) => note.id === parseInt(id));
+  const width = useWindowWidth();
+  const isMobile = width < 768;
+
+  const [subNoteText, setSubNoteText] = useState('');
+
+  if (!currentNote) {
+    return <div>Note not found</div>;
+  }
+
+  const handleSend = () => {
+    if (!subNoteText.trim()) return;
+    const newSubNote = {
+      text: subNoteText,
+      date,
+      time,
+    };
+    dispatch(addSubNote({ id: currentNote.id, subNote: newSubNote }));
+    setSubNoteText('');
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
+  return (
+    <div className="subnotes">
+      <div className="header">
+      {isMobile && (
+        <button onClick={() => navigate(-1)} className="back-button">
+          ←
+        </button>
+      )}
+        <div className="header-content"><Title title={currentNote.title} color={currentNote.color} /></div>
+      </div>
+      <div className="notes-content">
+        {currentNote.subNotes.map((subNote, index) => (
+          <Card key={index} text={subNote.text} date={subNote.date} time={subNote.time} />
+        ))}
+      </div>
+      <div className="textarea-foot">
+        <textarea
+          placeholder="Enter your text here..........."
+          value={subNoteText}
+          onChange={(e) => setSubNoteText(e.target.value)}
+          onKeyPress={handleKeyPress}
+        ></textarea>
+        <button
+          className={`send ${subNoteText.trim() ? 'enabled' : ''}`}
+          onClick={handleSend}
+          disabled={!subNoteText.trim()}
+        >
+          <IoSend />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SubNotes;
